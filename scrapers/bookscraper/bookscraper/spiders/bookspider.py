00aac6dc891ce066eee73b0f4b346a568905a0e7
dchung117 <dchung0330@gmail.com>
2023-06-17 18:55:44 -0400

    added automatic scrolling through pages until end.

--- scrapers/bookscraper/bookscraper/spiders/bookspider.py
+++ scrapers/bookscraper/bookscraper/spiders/bookspider.py
@@ -10,9 +10,18 @@ class BookspiderSpider(scrapy.Spider):
         # get all books from page
         books = response.css("article.product_pod")
 
+        # parse data
         for book in books:
             yield {
                 "title": book.css("h3 a::text").get(),
                 "price": book.css(".product_price .price_color::text").get(),
                 "url": book.css("h3 a").attrib["href"]
             }
+
+        # scroll to next page
+        next_page = response.css("li.next a::attr(href)").get()
+        if next_page is not None:
+            if not next_page.startswith("catalogue"):
+                next_page = "catalogue/" + next_page
+            next_page_url = "https://books.toscrape.com/" + next_page
+            yield response.follow(next_page_url, callback=self.parse)
